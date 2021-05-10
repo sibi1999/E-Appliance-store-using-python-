d677347c025d23e0cbdb54626717313cd721dbc8
Sibi <sksibi123@gmail.com>
2021-05-10 15:23:10 +0530

    check done for admin

--- Elec_Appliances/check.py
+++ Elec_Appliances/check.py
@@ -1,3 +1,38 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="12345sibi",
+  database="eapp"
+)
+
+mycursor = mydb.cursor()
+
+
+print("price and product grouping")
+sql = "select product,sum(price) from customer_table group by product;"
+mycursor.execute(sql)
+for i in mycursor:
+    print(i)
+
+
+print()
+print("price and date grouping")
+sql = "select product,sum(price) from customer_table group by date;"
+mycursor.execute(sql)
+for i in mycursor:
+    print(i)
+
+
+
+print()
+print("price and city grouping")
+sql = "select region,sum(price) from customer_table group by region;"
+mycursor.execute(sql)
+for i in mycursor:
+    print(i)
+'''
 from collections import Counter
 import json
 def counting_function(x):
@@ -18,4 +53,4 @@ print(df.groupby('date').sum())
 print()
 print()
 print(json.loads(df['product']))
-
+'''
